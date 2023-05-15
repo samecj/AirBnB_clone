98de1f04ec7f6048e9c4795ee38e9803617b85f0
samecj <chijiokesamuel6@gmail.com>
2023-05-15 04:43:13 +0000

    commited

---
+++ README.md
@@ -0,0 +1,1 @@
+this is air bnb project
---
+++ one/models/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''Initializes the package'''
+# from models.base_model import BaseModel
+from models.engine import file_storage
+
+
+storage = file_storage.FileStorage()
